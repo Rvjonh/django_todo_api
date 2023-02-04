48a057a244741299ca617ae16589b5031d767445
Rvjonh <jonhvelasco3@gmail.com>
2023-02-03 22:28:38 -0400

    Final project setup - Added Todo app with:     - urls todo_list and todo_detail     - Model of Todo with title and body     - serializer for Todo model     - Views with generic DRF - Readme and full specifications.

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,13 +40,23 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "rest_framework",
+    "corsheaders",
     # Local
     "todos.apps.TodosConfig",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -126,3 +136,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Configuration for django-cors-headers
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS").split(" ")
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS").split(" ")
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+from .models import Todo
+
+
+class TodoTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title="Learn about typescript",
+            body="Check freecodecamp course",
+        )
+        cls.todo2 = Todo.objects.create(
+            title="Second title",
+            body="Second Body",
+        )
+
+    def test_todo_model_content(self):
+        self.assertEqual(str(self.todo), "Learn about typescript")
+        self.assertEqual(self.todo.title, "Learn about typescript")
+        self.assertEqual(self.todo.body, "Check freecodecamp course")
+
+        self.assertEqual(str(self.todo2), "Second title")
+        self.assertEqual(self.todo2.title, "Second title")
+        self.assertEqual(self.todo2.body, "Second Body")
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("todo_list"))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 2)
+        self.assertContains(response, self.todo)
+        self.assertContains(response, self.todo2)
+
+    def test_api__detailview(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}), format="json"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, self.todo)
